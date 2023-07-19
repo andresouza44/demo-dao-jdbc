4fed89d8d3d169fd809b6519bc3cd01b547e2a3c
Andre M. de Souza <andresouza44@gmail.com>
2023-07-19 08:49:02 -0300

    Seller entity class.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Program {
     public static void main(String[] args) {
 
         Department dp = new Department(1,"Vendas");
-        System.out.println(dp);
+        Seller sell = new Seller(21,"MAria","marioa@gmail", LocalDate.now(),3000.,dp);
+
+        System.out.println(sell);
 
 
     }
