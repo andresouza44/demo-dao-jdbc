2cc015faca09dfa8dd0cb2f4a1ea5b51f1afe70b
Andre M. de Souza <andresouza44@gmail.com>
2023-07-19 08:24:04 -0300

    Department entity class.

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import models.entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department dp = new Department(1,"Vendas");
+        System.out.println(dp);
+
+
+    }
+}
